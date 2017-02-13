0077bbe83bb9d2ba0cab82a0ccf4c5402900fa92
rqh489 <Keira.Zhou@capitalone.com>
2017-02-13 16:26:08 -0500

    add more member fields

--- model/member.go
+++ model/member.go
@@ -2,6 +2,8 @@ package models
 
 // Member represents a member record.
 type Member struct {
-	Id   int    `json:"id" db:"id"`
-	Name string `json:"name" db:"name"`
+	Id        int    `json:"id" db:"id"`
+	FirstName string `json:"first_name" db:"first_name"`
+	LastName  string `json:"last_name" db:"last_name"`
+	Email     string `json:"email" db:"email"`
 }
